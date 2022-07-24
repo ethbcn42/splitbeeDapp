d978be3124a09203f70ba5bec4c2bcbae8feec42
javier cervilla <javier@MacBook-Pro-de-javier.local>
2022-07-24 02:08:22 +0200

    feat(CELO):[+] Add support for celo network

--- utils/constants.js
+++ utils/constants.js
@@ -8,7 +8,7 @@ export const network = {
 }
 
 export const child = {
-    abi: [{"inputs":[{"internalType":"address","name":"_admin","type":"address"},{"internalType":"uint256","name":"_percent","type":"uint256"},{"internalType":"address","name":"_ong","type":"address"},{"internalType":"address","name":"_factory","type":"address"},{"internalType":"address","name":"_wallet","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"remaining","type":"uint256"}],"name":"Transaction","type":"event"},{"inputs":[],"name":"admin","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32[]","name":"_proof","type":"bytes32[]"}],"name":"checkVerifyedONG","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"factory","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"ong","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"percent","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_admin","type":"address"}],"name":"setAdmin","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_ong","type":"address"},{"internalType":"bytes32[]","name":"_proof","type":"bytes32[]"}],"name":"setONG","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_percent","type":"uint256"}],"name":"setPercent","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_wallet","type":"address"}],"name":"setWallet","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"wallet","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"stateMutability":"payable","type":"receive"}]
+    abi: [{ "inputs": [{ "internalType": "address", "name": "_admin", "type": "address" }, { "internalType": "uint256", "name": "_percent", "type": "uint256" }, { "internalType": "address", "name": "_ong", "type": "address" }, { "internalType": "address", "name": "_factory", "type": "address" }, { "internalType": "address", "name": "_wallet", "type": "address" }], "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "from", "type": "address" }, { "indexed": true, "internalType": "address", "name": "to", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "amount", "type": "uint256" }, { "indexed": false, "internalType": "uint256", "name": "remaining", "type": "uint256" }], "name": "Transaction", "type": "event" }, { "inputs": [], "name": "admin", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "bytes32[]", "name": "_proof", "type": "bytes32[]" }], "name": "checkVerifyedONG", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "factory", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "ong", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "percent", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "_admin", "type": "address" }], "name": "setAdmin", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "_ong", "type": "address" }, { "internalType": "bytes32[]", "name": "_proof", "type": "bytes32[]" }], "name": "setONG", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "_percent", "type": "uint256" }], "name": "setPercent", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "_wallet", "type": "address" }], "name": "setWallet", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "wallet", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "stateMutability": "payable", "type": "receive" }]
 }
 
 export default {
@@ -21,7 +21,7 @@ export const networks = {
     137: {
         chainName: "Polygon",
         chainId: "0x89",
-        rpcUrls: ["polygon-rpc.com"],
+        rpcUrls: ["https://polygon.infura.io/v3/9db936720a2743b0a033001e1276fb60"],
         nativeCurrency: {
             name: "Matic",
             symbol: "Matic",
@@ -34,7 +34,7 @@ export const networks = {
     80001: {
         chainName: "Mumbai",
         chainId: "0x13881",
-        rpcUrls: ["https://rpc-mumbai.matic.today"],
+        rpcUrls: ["https://rpc-mumbai.maticvigil.com/v1/4a45a73c3839558b7642547f52c612ffcabeec49"],
         nativeCurrency: {
             name: "Matic",
             symbol: "Matic",
@@ -64,10 +64,23 @@ export const networks = {
         nativeCurrency: {
             name: "CRONOS",
             symbol: "CRO",
-            decimals: 18, 
+            decimals: 18,
         },
         blockExplorerUrls: ["https://cronoscan.com/"],
         logo: "/web3/cronos.svg",
         img: "/web3/cronos.png"
+    },
+    42220: {
+        chainId: "0xa4ec",
+        chainName: "Celo",
+        nativeCurrency: {
+            name: "Celo",
+            symbol: "CELO",
+            decimals: 18
+        },
+        rpcUrls: ["https://forno.celo.org"],
+        blockExplorerUrls: ["https://explorer.celo.org/"],
+        logo: "/web3/celo.svg",
+        img: "/web3/celo.png"
     }
 }
